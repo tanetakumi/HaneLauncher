366b93aacdbd6d3fa89cd3d8d49cd61f2f971de7
tanetakumi <tanetakumi@gmail.com>
2022-11-29 09:47:18 +0900

    Change url of release notes

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1444,7 +1444,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/tanetakumi/HaneLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
